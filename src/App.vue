90580a1adec5bbfbe027e8a6b7fbd07ccc42d9e6
Jacky Nguyen <cutii@DESKTOP-B3LCIQI>
2022-04-05 13:40:42 +0700

    Update project to chat with firestore

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="login" v-if="!isLogin">
+			<div class="enter-username">
+				<input type="text" placeholder="Username" v-model="email" />
+			</div>
+			<div class="enter-password">
+				<input type="text" placeholder="password" v-model="password" />
+			</div>
+			<button @click="register()">Register</button>
+			<button @click="login()">Login</button>
+		</div>
+		<div class="chat-content" v-else>
+			<h1>Hello: {{ email }}</h1>
+			<div class="enter-message">
+				<input type="text" placeholder="Message" v-model="message" />
+				<button @click="sendMessage">Send</button>
+			</div>
+			<ul class="list-message" v-if="listMesssage.length">
+				<li class="item-message" v-for="m in listMesssage" :key="m.id">
+					<strong>{{ m.data.from }}</strong> : <i>{{ m.data.message }}</i>
+				</li>
+			</ul>
+
+			<button @click="logout()">Logout</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import {
+		collection,
+		onSnapshot,
+		addDoc,
+		query,
+		orderBy,
+	} from 'firebase/firestore';
+	import { db, register, login, auth } from './firebase';
+	export default {
+		name: 'App',
+		components: {},
+		data() {
+			return {
+				message: null,
+				listMesssage: [],
+				collectionDB: null,
+				isRegister: false,
+				isLogin: false,
+				email: '',
+				password: '',
+			};
+		},
+		watch: {},
+		methods: {
+			logout() {
+				auth
+					.signOut()
+					.then(() => {})
+					.catch((e) => {
+						console.log(e.code + ' ' + e.message);
+					});
+			},
+			async login() {
+				login(this.email, this.password)
+					.then((res) => {
+						console.log(res);
+					})
+					.catch((e) => {
+						console.log(e);
+					});
+			},
+			async register() {
+				register(this.email, this.password);
+			},
+			async init() {
+				auth.onAuthStateChanged((user) => {
+					this.isLogin = user ? ((this.email = user.email), true) : false;
+				});
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+				this.collectionDB = collection(db, 'chat');
+				var vm = this;
+				var q = query(this.collectionDB, orderBy('createdAt'));
+
+				onSnapshot(q, (querySnapshot) => {
+					let newMessages = [];
+					querySnapshot.forEach((snapshot) => {
+						let id = snapshot.id;
+						let newMessage = {
+							id: id,
+							data: snapshot.data(),
+						};
+						newMessages.push(newMessage);
+					});
+					// vm.listMesssage = newMessages;
+					vm.listMesssage = newMessages;
+				});
+			},
+			sendMessage() {
+				const vm = this;
+				if (this.message) {
+					addDoc(this.collectionDB, {
+						message: this.message,
+						createdAt: new Date(),
+						from: this.email,
+					})
+						.then((res) => {
+							console.log('res ', res);
+							vm.message = null;
+						})
+						.catch((e) => {
+							console.log('error: ' + e);
+						});
+				}
+			},
+		},
+		created() {
+			this.init();
+		},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	body {
+		margin-top: 20px;
+		background: #ebeef0;
+	}
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,77 @@
+import { initializeApp } from 'firebase/app';
+// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { getFirestore } from 'firebase/firestore';
+import {
+	AuthCredential,
+	createUserWithEmailAndPassword,
+	getAuth,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBUtGmYuvAYIxvzbHMp_AS7FdzUl4MpWpI',
+	authDomain: 'pengpeng-330303.firebaseapp.com',
+	projectId: 'pengpeng-330303',
+	storageBucket: 'pengpeng-330303.appspot.com',
+	messagingSenderId: '488183996768',
+	appId: '1:488183996768:web:2821d5920799cd9951fe05',
+	measurementId: 'G-PCE3ELJG8H',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+const register = function (email, password) {
+	createUserWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			console.log('Register success ' + userCredential.user);
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+};
+
+const login = function (email, password) {
+	return new Promise((resolve, reject) => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				resolve(userCredential.user);
+			})
+			.catch((e) => {
+				reject(e);
+			});
+	});
+};
+
+export { db, register, login, auth };
+
+// const messaging = getMessaging();
+// getToken(messaging, {
+// 	vapidKey:
+// 		'BLkPkqRdH-GfAvBmUvDz2npXxRjwS-6-MWE2s8jA1N-lRVJWnmUS3IG50XZ7jzU_RGYXGBzLSMhGdfo8cTg9xpg',
+// })
+// 	.then((currentToken) => {
+// 		if (currentToken) {
+// 			console.log(currentToken);
+// 		} else {
+// 			// Show permission request UI
+// 			console.log(
+// 				'No registration token available. Request permission to generate one.'
+// 			);
+// 			// ...
+// 		}
+// 	})
+// 	.catch((err) => {
+// 		console.log('An error occurred while retrieving token. ', err);
+// 		// ...
+// 	});
+
+// onMessage(messaging, (payload) => {
+// 	alert('Bạn vừa có 1 tin: ', payload.notification.title);
+// });
+
+// export default messaging;
